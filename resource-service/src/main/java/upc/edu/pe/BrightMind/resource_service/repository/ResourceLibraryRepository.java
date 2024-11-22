daca93f9feff6cfb87d174b13bcca7e1f650163f
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2024-11-22 04:13:05 -0500

    Update ResourceLibraryRepository.java

--- resource-service/src/main/java/upc/edu/pe/BrightMind/resource_service/repository/ResourceLibraryRepository.java
+++ resource-service/src/main/java/upc/edu/pe/BrightMind/resource_service/repository/ResourceLibraryRepository.java
@@ -1,8 +1,11 @@
 package upc.edu.pe.BrightMind.resource_service.repository;
 
 
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import upc.edu.pe.BrightMind.resource_service.model.dtos.ResourceLibraryMinimalDTO;
 import upc.edu.pe.BrightMind.resource_service.model.entities.Grade;
 import upc.edu.pe.BrightMind.resource_service.model.entities.ResourceLibrary;
 import upc.edu.pe.BrightMind.resource_service.model.entities.Subject;
@@ -20,4 +23,13 @@ public interface ResourceLibraryRepository extends JpaRepository<ResourceLibrary
     List<ResourceLibrary> findBySubject(Subject subject);
 
     List<ResourceLibrary> findByGradeAndSubject(Grade grade, Subject subject);
+
+    @Query("SELECT new upc.edu.pe.BrightMind.resource_service.model.dtos.ResourceLibraryMinimalDTO(r.id, r.title, r.subject, r.grade) FROM ResourceLibrary r WHERE r.grade = :grade")
+    List<ResourceLibraryMinimalDTO> findByGradeExcludePdf(@Param("grade") Grade grade);
+
+    @Query("SELECT new upc.edu.pe.BrightMind.resource_service.model.dtos.ResourceLibraryMinimalDTO(r.id, r.title, r.subject, r.grade) FROM ResourceLibrary r WHERE r.subject = :subject")
+    List<ResourceLibraryMinimalDTO> findBySubjectExcludePdf(@Param("subject") Subject subject);
+
+    @Query("SELECT new upc.edu.pe.BrightMind.resource_service.model.dtos.ResourceLibraryMinimalDTO(r.id, r.title, r.subject, r.grade) FROM ResourceLibrary r WHERE r.grade = :grade AND r.subject = :subject")
+    List<ResourceLibraryMinimalDTO> findByGradeAndSubjectExcludePdf(@Param("grade") Grade grade, @Param("subject") Subject subject);
 }
